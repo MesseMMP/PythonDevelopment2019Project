30c8f5454a6d6019e9ca80e70edcc5233fa76c4f
Chocolate queen <NUT1997020501@yandex.ru>
2019-04-28 23:10:32 +0300

    Исправлено место расположения canvas

--- mainpage.py
+++ mainpage.py
@@ -1,7 +1,7 @@
 import time
 from tkinter import *
 from threading import Thread, Event
-
+'''
 class Clock(Thread):
     def __init__(self, root, grain=1, event="<<Tick>>"):
         super().__init__()
@@ -13,23 +13,27 @@ class Clock(Thread):
     def run(self):
         while not self.done.wait(self.grain):
             self.root.event_generate(self.event)
+            '''
 
 class AppBase(Frame):
     def __init__(self, master=None, **kwargs):
         Frame.__init__(self, master, **kwargs)
         self.master.rowconfigure(0, weight = 1)
-        self.master.rowconfigure(6, weight = 1)
         self.master.columnconfigure(0, weight = 1)
-        self.grid()
+        self.grid(sticky=N+E+S+W)
+        self.rowconfigure(0, weight=2)
+        self.rowconfigure(6, weight=2)
+        self.columnconfigure(0, weight=2)
         self.create()
 
+'''
     def start(self):
         self.Clock.start()
 
     def quit(self, *events):
         self.Clock.done.set()
         self.master.quit()
-
+'''
 class Area(Canvas):
     def buttondown(*args):
         print(*args)
@@ -41,19 +45,20 @@ class Area(Canvas):
 class App(AppBase):
     def create(self):
         self.Canvas = Area(self)
-        self.Canvas.grid(row=0, column=0, sticky = N+E+S+W, rowspan=7)
+        self.Canvas.grid(row=0, column=0, rowspan=7, sticky = N+E+S+W)
+        self.Canvas.config(bg='black')
         self.create_buttons()
 
     def create_buttons(self):
-        self.Start = Button(text="Start")
+        self.Start = Button(self, text="Start")
         self.Start.grid(row=1, column=1, sticky=E+W, padx=5, pady=7)
-        self.Stop = Button(text="Stop")
+        self.Stop = Button(self, text="Stop")
         self.Stop.grid(row=2, column=1, sticky=E+W, padx=5, pady=7)
-        self.Step = Button(text="Step")
+        self.Step = Button(self, text="Step")
         self.Step.grid(row=3, column=1, sticky=E+W, padx=5, pady=7)
-        self.Clear = Button(text="Clear")
+        self.Clear = Button(self, text="Clear")
         self.Clear.grid(row=4, column=1, sticky=E+W, padx=5, pady=7)
-        self.AddPattern = Button(text="AddPattern")
+        self.AddPattern = Button(self, text="AddPattern")
         self.AddPattern.grid(row=5, column=1, sticky=E+W, padx=5, pady=7)
 
 
