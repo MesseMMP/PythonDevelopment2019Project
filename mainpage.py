6165d7659f835d49c678f63c1ba1ff43d5627caf
Chocolate queen <NUT1997020501@yandex.ru>
2019-04-29 00:12:34 +0300

    Добавлена сетка на игровую площадку

--- mainpage.py
+++ mainpage.py
@@ -41,13 +41,16 @@ class Area(Canvas):
     def __init__(self, master=None):
         Canvas.__init__(self, master)
         self.bind("<Button-1>", self.buttondown)
+    
 
 class App(AppBase):
     def create(self):
         self.Canvas = Area(self)
         self.Canvas.grid(row=0, column=0, rowspan=7, sticky = N+E+S+W)
-        self.Canvas.config(bg='black')
+        self.Canvas.update()
+        #self.Canvas.bind("<Configure>", self.create_area)
         self.create_buttons()
+        self.create_area()
 
     def create_buttons(self):
         self.Start = Button(self, text="Start")
@@ -60,6 +63,23 @@ class App(AppBase):
         self.Clear.grid(row=4, column=1, sticky=E+W, padx=5, pady=7)
         self.AddPattern = Button(self, text="AddPattern")
         self.AddPattern.grid(row=5, column=1, sticky=E+W, padx=5, pady=7)
+
+
+    def create_area(self):
+        area_width = self.Canvas.winfo_width()
+        area_height = self.Canvas.winfo_height()
+        delta_x = int((area_width-5)/50)
+        start_x = 2
+        finish_x = area_width-3
+        delta_y = int((area_height-5)/30)
+        start_y = 2
+        finish_y = area_height-3
+        self.Canvas.create_rectangle(start_x, start_y, finish_x, finish_y, fill='white')
+        for x in range(start_x+delta_x, finish_x, delta_x):
+            self.Canvas.create_line(x, start_y+1, x, finish_y, fill='lightgray')
+        for y in range(start_y+delta_y, finish_y, delta_y):
+            self.Canvas.create_line(start_x+1, y, finish_x, y, fill='lightgray')
+
 
 
 Tick = App()
