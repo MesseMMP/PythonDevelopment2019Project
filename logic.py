50f385664286cdaafe33148591d735bd81a4c7aa
andrew <sudoandros@github.com>
2019-05-17 20:03:27 +0300

    Refactor the code

--- logic.py
+++ logic.py
@@ -1,47 +1,9 @@
 import doctest
 
 
-def one_step_matrix(matrix):
-    '''Processing of one step with the return of the area matrix
-    '''
-    columns = len(matrix[0])
-    rows = len(matrix)
-    new_matrix = [[0 for i in range(columns)] for j in range(rows)]
-    for row, row_cells in enumerate(matrix):
-        for col, cell in enumerate(row_cells):
-            num_life = 0
-            if row:
-                if col:
-                    num_life += 1 if matrix[row-1][col-1] else 0
-                if col < columns-1:
-                    num_life += 1 if matrix[row-1][col+1] else 0
-                num_life += 1 if matrix[row-1][col] else 0
-            if col:
-                if row < rows-1:
-                    num_life += 1 if matrix[row+1][col-1] else 0
-                num_life += 1 if matrix[row][col-1] else 0
-            if row < rows-1:
-                num_life += 1 if matrix[row+1][col] else 0
-                if col < columns-1:
-                    num_life += 1 if matrix[row+1][col+1] else 0
-            if col < columns-1:
-                num_life += 1 if matrix[row][col+1] else 0
-            if cell:
-                if num_life == 2 or num_life == 3:
-                    new_matrix[row][col] = 1
-                else:
-                    new_matrix[row][col] = 0
-            else:
-                if num_life == 3:
-                    new_matrix[row][col] = 1
-                else:
-                    new_matrix[row][col] = 0
-    return new_matrix
-
-
 def one_step_life_dead(matrix):
-    '''Processing of one step with the return of the life and dead lists and area matrix
-    '''
+    """Processing of one step with the return of the life and dead lists and area matrix
+    """
     columns = len(matrix[0])
     rows = len(matrix)
     life_list = []
@@ -52,20 +14,20 @@ def one_step_life_dead(matrix):
             num_life = 0
             if row:
                 if col:
-                    num_life += 1 if matrix[row-1][col-1] else 0
-                if col < columns-1:
-                    num_life += 1 if matrix[row-1][col+1] else 0
-                num_life += 1 if matrix[row-1][col] else 0
+                    num_life += 1 if matrix[row - 1][col - 1] else 0
+                if col < columns - 1:
+                    num_life += 1 if matrix[row - 1][col + 1] else 0
+                num_life += 1 if matrix[row - 1][col] else 0
             if col:
-                if row < rows-1:
-                    num_life += 1 if matrix[row+1][col-1] else 0
-                num_life += 1 if matrix[row][col-1] else 0
-            if row < rows-1:
-                num_life += 1 if matrix[row+1][col] else 0
-                if col < columns-1:
-                    num_life += 1 if matrix[row+1][col+1] else 0
-            if col < columns-1:
-                num_life += 1 if matrix[row][col+1] else 0
+                if row < rows - 1:
+                    num_life += 1 if matrix[row + 1][col - 1] else 0
+                num_life += 1 if matrix[row][col - 1] else 0
+            if row < rows - 1:
+                num_life += 1 if matrix[row + 1][col] else 0
+                if col < columns - 1:
+                    num_life += 1 if matrix[row + 1][col + 1] else 0
+            if col < columns - 1:
+                num_life += 1 if matrix[row][col + 1] else 0
             if cell:
                 if num_life == 2 or num_life == 3:
                     new_matrix[row][col] = 1
--- mainpage.py
+++ mainpage.py
@@ -3,20 +3,6 @@ import logic as lg
 from tkinter import Frame, Canvas, N, E, S, W, Button
 from threading import Thread, Event
 
-"""
-class Clock(Thread):
-    def __init__(self, root, grain=1, event="<<Tick>>"):
-        super().__init__()
-        self.root = root    # Окно, которому посылать событие
-        self.grain = grain  # Размер одного тика в секундах (м. б дробный)
-        self.event = event  # TKinter-событие, которое надо посылать
-        self.done = Event() # threading-событие, которое останавливет тред
-
-    def run(self):
-        while not self.done.wait(self.grain):
-            self.root.event_generate(self.event)
-            """
-
 
 class AppBase(Frame):
     def __init__(self, master=None, **kwargs):
@@ -32,58 +18,34 @@ class AppBase(Frame):
     def create(self):
         raise NotImplementedError
 
-
-"""
-    def start(self):
-        self.Clock.start()
-
-    def quit(self, *events):
-        self.Clock.done.set()
-        self.master.quit()
-"""
-
-
-class Area(Canvas):
+class GameGrid(Canvas):
     def __init__(self, master=None):
         Canvas.__init__(self, master)
         self.bind("<Button-1>", self.buttondown)
-        self.delta_x = 15
-        self.delta_y = 15
+        self.cell_width = 15
+        self.cell_height = 15
         self.start_x = 2
         self.start_y = 2
 
     def buttondown(self, event):
         left_border_cell = event.x
         top_border_cell = event.y
-        while (left_border_cell - self.start_x) % self.delta_x:
+        while (left_border_cell - self.start_x) % self.cell_width:
             left_border_cell -= 1
-        while (top_border_cell - self.start_y) % self.delta_y:
+        while (top_border_cell - self.start_y) % self.cell_height:
             top_border_cell -= 1
-        row = (top_border_cell - self.start_y) // self.delta_y
-        column = (left_border_cell - self.start_x) // self.delta_x
+        row = (top_border_cell - self.start_y) // self.cell_height
+        column = (left_border_cell - self.start_x) // self.cell_width
         if matrix_grid[row][column]:
             matrix_grid[row][column] = 0
-            self.create_rectangle(
-                left_border_cell + 1,
-                top_border_cell + 1,
-                left_border_cell + self.delta_x - 1,
-                top_border_cell + self.delta_y - 1,
-                fill="white",
-                outline="white",
-            )
+            self._draw_dead_cell(row, column)
         else:
             matrix_grid[row][column] = 1
-            self.create_rectangle(
-                left_border_cell + 1,
-                top_border_cell + 1,
-                left_border_cell + self.delta_x - 1,
-                top_border_cell + self.delta_y - 1,
-                fill="black",
-            )
+            self._draw_alive_cell(row, column)
 
     def create_grid(self):
-        self.area_width = COLUMN_MATRIX * self.delta_x
-        self.area_height = ROW_MATRIX * self.delta_y
+        self.area_width = COLUMN_MATRIX * self.cell_width
+        self.area_height = ROW_MATRIX * self.cell_height
         self.finish_x = self.area_width - 3
         self.finish_y = self.area_height - 3
         self.create_rectangle(
@@ -94,97 +56,76 @@ class Area(Canvas):
             fill="white",
             outline="white",
         )
-        for x in range(self.start_x + self.delta_x, self.finish_x, self.delta_x):
+        for x in range(self.start_x + self.cell_width, self.finish_x, self.cell_width):
             self.create_line(x, self.start_y + 1, x, self.finish_y, fill="lightgray")
-        for y in range(self.start_y + self.delta_y, self.finish_y, self.delta_y):
+        for y in range(
+            self.start_y + self.cell_height, self.finish_y, self.cell_height
+        ):
             self.create_line(self.start_x + 1, y, self.finish_x, y, fill="lightgray")
 
-    def draw_cells(self, matrix):
-        for row, row_cells in enumerate(matrix):
-            for col, cell in enumerate(row_cells):
-                if cell:
-                    self.create_rectangle(
-                        self.start_x + col * self.delta_x + 1,
-                        self.start_y + row * self.delta_y + 1,
-                        self.start_x + (col + 1) * self.delta_x - 1,
-                        self.start_y + (row + 1) * self.delta_y - 1,
-                        fill="black",
-                    )
-                else:
-                    self.create_rectangle(
-                        self.start_x + col * self.delta_x + 1,
-                        self.start_y + row * self.delta_y + 1,
-                        self.start_x + (col + 1) * self.delta_x - 1,
-                        self.start_y + (row + 1) * self.delta_y - 1,
-                        fill="white",
-                        outline="white",
-                    )
-
     def draw_cells_life_dead(self, life, dead):
-        for row, col in life:
-            self.create_rectangle(
-                self.start_x + col * self.delta_x + 1,
-                self.start_y + row * self.delta_y + 1,
-                self.start_x + (col + 1) * self.delta_x - 1,
-                self.start_y + (row + 1) * self.delta_y - 1,
-                fill="black",
-            )
-        for row, col in dead:
-            self.create_rectangle(
-                self.start_x + col * self.delta_x + 1,
-                self.start_y + row * self.delta_y + 1,
-                self.start_x + (col + 1) * self.delta_x - 1,
-                self.start_y + (row + 1) * self.delta_y - 1,
-                fill="white",
-                outline="white",
-            )
+        for row, column in life:
+            self._draw_alive_cell(row, column)
+        for row, column in dead:
+            self._draw_dead_cell(row, column)
 
     def clear(self, matrix):
         new_matrix = []
-        for i in range(ROW_MATRIX):
-            new_matrix.append([0 for j in range(COLUMN_MATRIX)])
+        new_matrix = [[0 for i in range(COLUMN_MATRIX)] for j in range(ROW_MATRIX)]
         for row, rows_cells in enumerate(matrix):
-            for col, cell in enumerate(rows_cells):
+            for column, cell in enumerate(rows_cells):
                 if cell:
-                    self.create_rectangle(
-                        self.start_x + col * self.delta_x + 1,
-                        self.start_y + row * self.delta_y + 1,
-                        self.start_x + (col + 1) * self.delta_x - 1,
-                        self.start_y + (row + 1) * self.delta_y - 1,
-                        fill="white",
-                        outline="white",
-                    )
+                    self._draw_dead_cell(row, column)
         return new_matrix
+
+    def _draw_alive_cell(self, row, column):
+        self.create_rectangle(
+            self.start_x + column * self.cell_width + 1,
+            self.start_y + row * self.cell_height + 1,
+            self.start_x + (column + 1) * self.cell_width - 1,
+            self.start_y + (row + 1) * self.cell_height - 1,
+            fill="black",
+        )
+
+    def _draw_dead_cell(self, row, column):
+        self.create_rectangle(
+            self.start_x + column * self.cell_width + 1,
+            self.start_y + row * self.cell_height + 1,
+            self.start_x + (column + 1) * self.cell_width - 1,
+            self.start_y + (row + 1) * self.cell_height - 1,
+            fill="white",
+            outline="white",
+        )
 
 
 class App(AppBase):
     def create(self):
-        self.Canvas = Area(self)
-        self.Canvas.grid(row=0, column=0, rowspan=7, sticky=N + E + S + W)
-        self.Canvas.update()
-        self.Canvas.create_grid()
+        self.game_grid = GameGrid(self)
+        self.game_grid.grid(row=0, column=0, rowspan=7, sticky=N + E + S + W)
+        self.game_grid.update()
+        self.game_grid.create_grid()
         self.create_buttons()
 
     def create_buttons(self):
-        self.Start = Button(self, text="Start")
-        self.Start.grid(row=1, column=1, sticky=E + W, padx=5, pady=7)
-        self.Stop = Button(self, text="Stop")
-        self.Stop.grid(row=2, column=1, sticky=E + W, padx=5, pady=7)
-        self.Step = Button(self, text="Step", command=self.one_step)
-        self.Step.grid(row=3, column=1, sticky=E + W, padx=5, pady=7)
-        self.Clear = Button(self, text="Clear", command=self.clear)
-        self.Clear.grid(row=4, column=1, sticky=E + W, padx=5, pady=7)
-        self.AddPattern = Button(self, text="AddPattern")
-        self.AddPattern.grid(row=5, column=1, sticky=E + W, padx=5, pady=7)
+        self.start_button = Button(self, text="Start")
+        self.start_button.grid(row=1, column=1, sticky=E + W, padx=5, pady=7)
+        self.stop_button = Button(self, text="Stop")
+        self.stop_button.grid(row=2, column=1, sticky=E + W, padx=5, pady=7)
+        self.step_button = Button(self, text="Step", command=self.one_step)
+        self.step_button.grid(row=3, column=1, sticky=E + W, padx=5, pady=7)
+        self.clear_button = Button(self, text="Clear", command=self.clear)
+        self.clear_button.grid(row=4, column=1, sticky=E + W, padx=5, pady=7)
+        self.add_pattern_button = Button(self, text="AddPattern")
+        self.add_pattern_button.grid(row=5, column=1, sticky=E + W, padx=5, pady=7)
 
     def one_step(self):
         global matrix_grid
         life_list, dead_list, matrix_grid = lg.one_step_life_dead(matrix_grid)
-        self.Canvas.draw_cells_life_dead(life_list, dead_list)
+        self.game_grid.draw_cells_life_dead(life_list, dead_list)
 
     def clear(self):
         global matrix_grid
-        matrix_grid = self.Canvas.clear(matrix_grid)
+        matrix_grid = self.game_grid.clear(matrix_grid)
 
 
 ROW_MATRIX = 150
